cd6a27b2f0b756e4f626c92e697aa85b54a59d8c
AnnaPicha <anna.picha@gmail.com>
2025-05-16 10:57:55 +0200

    adding property script

--- condensed_phase_properties/analysis/cp_props.py
+++ condensed_phase_properties/analysis/cp_props.py
@@ -1,8 +1,143 @@
-# cp_props.py
-def process_data(simulated_data):
-    """
-    Processes the simulated data for analysis.
-    """
-    print(f"Processing simulated data: {simulated_data}")
-    analyzed_data = simulated_data["result"] * 2
-    return analyzed_data
+import pandas as pd
+import numpy as np
+from openff.units import unit
+
+
+
+R   = 8.31446261815324 # gas constant in J/mol/Kelvin
+k_B = 1.380649 *10**(-23) #J/K
+
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# define NPT properties -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def calc_heat_capacity_units(total_energy: float, number_particles : int, temp : float, molar_mass : float, printing : bool):
+    gas_constant = 8.31446261815324 * unit.joule / unit.mole / unit.kelvin #J * mol-1 * K-1
+    pot_var = total_energy.var() * (unit.kilojoule / unit.mole)**2
+    temp = temp*unit.kelvin
+
+    val = pot_var/number_particles/temp**2/gas_constant #factor 10**6 converts kj from total_energy data to J to match unit of the gas constant
+    val = val.to(unit.cal / unit.mole / unit.kelvin) / molar_mass
+    #heat_capacity["ani2x"].to(unit.cal / unit.mole / unit.kelvin)/molar_mass_water
+    if (printing):
+        print("heat capacity: ", val)
+    return val
+
+def calc_thermal_expansion(total_energy: float, volume: float, temp: float, printing : bool):
+    cov_en_vol = np.cov(total_energy,volume)[0][1]*(unit.nanometer**3)* unit.kJ / unit.mole
+    gas_constant=R*unit.joule/unit.mole/unit.kelvin
+    T=temp*unit.kelvin
+    volume=volume.mean()*(unit.nanometer**3)
+    alpha = cov_en_vol/gas_constant/T**2/volume
+    alpha_shift=alpha.to(1/unit.kelvin)
+    if (printing):
+        print("thermal expansion: ", alpha_shift)
+    return alpha_shift
+
+def calc_isothermal_compressibility(volume : float, temp : float, printing : bool):
+    volume_var = volume.var()*(unit.nanometer**3)**2
+    volume_mean = volume.mean()*(unit.nanometer**3)
+    boltzmann_constant=k_B*unit.joule/unit.kelvin
+    T=temp*unit.kelvin
+
+    val = volume_var/boltzmann_constant/T/volume_mean
+    val = val.to(1/unit.bar)
+    if (printing):
+        print("thermal expansion: ", val)
+    return val
+
+def calc_heat_of_vaporization (pot_energy: float, pot_energy_mono: float, temp_traj: float, box_count : int, printing: bool):
+    gas_constant = 8.31446261815324 * unit.joule / unit.mole / unit.kelvin #J * mol-1 * K-1if system=="moh323":
+    pot_mean = pot_energy.mean() * unit.kilojoule/unit.mole / box_count
+    pot_mono_mean = pot_energy_mono.mean() * unit.kilojoule/unit.mole
+    temp_mean = temp_traj.mean() * unit.kelvin
+    val =  pot_mono_mean - pot_mean + gas_constant * temp_mean
+    if (printing):
+        print("heat of vaporozation: ", val)
+    return val
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# define bootstrapping functions ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+def my_bootstrap_hov(liquid_pot : float, mono_pot : float, liquid_temp : float, Nboot : int, statfun):
+    '''Calculate bootstrap statistics for a sample x'''
+    liquid_pot=np.array(liquid_pot)
+    liquid_temp=np.array(liquid_temp)
+    mono_pot = np.array(mono_pot)
+
+    resampled_stat = []
+    for k in range(Nboot):
+        index = np.random.randint(0,len(liquid_pot),len(liquid_pot))
+        sample_pot = liquid_pot[index]
+        sample_temp = liquid_temp[index]
+        bastatistics = statfun(sample_pot, mono_pot, sample_temp, False).magnitude
+        resampled_stat.append(bastatistics)
+
+    return np.array(resampled_stat)
+
+def my_bootstrap_hcap(liquid_total : float, box_count : int, liquid_temp : float, Nboot : int, statfun):
+    '''Calculate bootstrap statistics for a sample x'''
+    liquid_total=np.array(liquid_total)
+    liquid_temp=np.array(liquid_temp)
+
+    resampled_stat = []
+    for k in range(Nboot):
+        index = np.random.randint(0,len(liquid_total),len(liquid_total))
+        sample_pot = liquid_total[index]
+        sample_temp = liquid_temp[index]
+        bastatistics = statfun(sample_pot, box_count, sample_temp.mean(), False).magnitude
+        resampled_stat.append(bastatistics)
+
+    return np.array(resampled_stat)
+
+def my_bootstrap_texp(liquid_total : float, box_vol : float, liquid_temp : float, Nboot : int, statfun):
+    '''Calculate bootstrap statistics for a sample x'''
+    liquid_total=np.array(liquid_total)
+    box_vol=np.array(box_vol)
+    liquid_temp=np.array(liquid_temp)
+
+    resampled_stat = []
+    for k in range(Nboot):
+        index = np.random.randint(0,len(liquid_total),len(liquid_total))
+        sample_pot = liquid_total[index]
+        sample_box_vol = box_vol[index]
+        sample_temp = liquid_temp[index]
+        bastatistics = statfun(sample_pot, sample_box_vol, sample_temp.mean(), False).magnitude
+        resampled_stat.append(bastatistics)
+
+    return np.array(resampled_stat)
+
+def my_bootstrap_icomp(box_vol : float, liquid_temp : float, Nboot : int, statfun):
+    '''Calculate bootstrap statistics for a sample x'''
+    box_vol=np.array(box_vol)
+    liquid_temp=np.array(liquid_temp)
+
+    resampled_stat = []
+    for k in range(Nboot):
+        index = np.random.randint(0,len(box_vol),len(box_vol))
+        sample_box_vol = box_vol[index]
+        sample_temp = liquid_temp[index]
+        icomp = statfun(sample_box_vol, sample_temp.mean(), False).magnitude
+        resampled_stat.append(icomp)
+
+    return np.array(resampled_stat)
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
